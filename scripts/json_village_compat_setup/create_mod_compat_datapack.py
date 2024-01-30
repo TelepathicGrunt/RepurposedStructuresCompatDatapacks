adfde47e2db46c680019fb8f90e267cc78b76686
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-01-30 07:25:30 -0500

    Added script to premake new project

---
+++ scripts/json_village_compat_setup/create_mod_compat_datapack.py
@@ -0,0 +1,141 @@
+from os.path import exists
+import os
+import json
+import shutil
+import re
+
+datapack_location = "C:\Users\MSI Laptop\Documents\ModWorkspace\RepurposedStructuresCompatDatapacks"
+
+def createFile(input_path, output_path, regex_list):
+    file_content = ''
+    with open(input_path, 'r') as file:
+        file_content = file.read()
+        for i in range(len(regex_list)):
+           file_content = file_content.replace(f'${i + 1}', regex_list[i])
+
+    path = os.path.join(datapack_location, output_path)
+    if not exists(path):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, 'w') as codefile1:
+            codefile1.write(file_content)
+        
+
+def createPoolFile(input_path, output_path, regex_list):
+    file_content = ''
+    with open(input_path, 'r') as file:
+        file_content = file.read()
+        for i in range(len(regex_list)):
+           file_content = file_content.replace(f'${i + 1}', regex_list[i])
+
+    path = os.path.join(datapack_location, output_path)
+    if not exists(path):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, 'w') as codefile1:
+            codefile1.write(file_content)
+        
+
+#--------------------------------------------------------------------------------------------
+
+restart = True
+while restart:
+    
+    mod_name = input("\nThe name of the mod to make compat with\n").strip()
+    mod_namespace = input("\nThe modid of the mod to make compat with\n").strip()
+    
+    n = int(input("\nEnter number of pieces per village:\n"))
+    piece_names = []
+    for i in range(0, n):
+        piece_names.append(input("\nName of the nbt file\n").strip())
+
+    structure_type = "village"
+    structure_type_plural = structure_type if structure_type.endswith('s') else ((structure_type[:-1] + 'ies') if structure_type.endswith('y') else (structure_type + 's'));
+    structure_variants = {
+        "badlands": "repurposed_structures:villages/badlands/randomizer",
+        "bamboo": "repurposed_structures:villages/bamboo/mossify",
+        "birch": "repurposed_structures:villages/birch/mossify",
+        "cherry": "repurposed_structures:villages/cherry/mossify",
+        "crimson": "repurposed_structures:villages/crimson/randomizer",
+        "dark_forest": "repurposed_structures:villages/dark_forest/mossify",
+        "giant_taiga": "repurposed_structures:villages/giant_taiga/mossify",
+        "jungle": "repurposed_structures:villages/jungle/mossify",
+        "mountains": "repurposed_structures:villages/mountains/mossify",
+        "mushroom": "repurposed_structures:villages/mushroom/general_randomizer",
+        "oak": "repurposed_structures:villages/oak/mossify",
+        "ocean": "repurposed_structures:villages/ocean/randomize",
+        "swamp": "repurposed_structures:villages/swamp/mossify",
+        "warped": "repurposed_structures:villages/warped/randomizer"
+    }
+
+    folderName = f"{datapack_location}\\Compat_{mod_name.replace(' ', '_')}"
+    data_location = f"{folderName}\\src\\data"
+
+    #-------------------------------------------------------------------------------------------
+    
+    # Setup project
+    root_src_dir = os.path.join('template', 'Compat_Blank')
+    for src_dir, dirs, files in os.walk(root_src_dir):
+        dst_dir = src_dir.replace(root_src_dir, folderName, 1)
+        if not os.path.exists(dst_dir):
+            os.makedirs(dst_dir)
+        for file_ in files:
+            src_file = os.path.join(src_dir, file_)
+            dst_file = os.path.join(dst_dir, file_)
+            if os.path.exists(dst_file):
+                os.remove(dst_file)
+            shutil.copy(src_file, dst_dir)
+
+
+    # Gradle properties
+    with open(os.path.join(folderName, 'gradle.properties'), 'r' ) as input:
+        content = input.read()
+
+        content_new = re.sub('$1', rf'repurposed_structures_{mod_namespace}', content, flags = re.M)
+        content_new = re.sub('$2', rf'repurposed_structures_{mod_name}', content, flags = re.M)
+        content_new = re.sub('$3', rf'{mod_namespace}', content, flags = re.M)
+            
+        with open(os.path.join(folderName, 'gradle.properties'), 'w' ) as output:
+            output.write(content_new)
+
+
+    # Datapack json files
+    for variant in structure_variants:
+        path = os.path.join(os.path.join('template', 'piece_count.json'))
+        with open(path, 'r+') as file:
+            jsonData = json.load(file)
+            for piece_name in piece_names:
+                jsonData['pieces_spawn_counts'].append({
+                    "nbt_piece_name" : f"{mod_namespace}:villages/{variant}/{piece_name}",
+                    "never_spawn_more_than_this_many": 1
+                })
+            path = os.path.join(datapack_location, os.path.join(data_location, 'repurposed_structures', 'rs_pieces_spawn_counts_additions', f'{structure_type}_{variant}.json'))
+            if not exists(path):
+                os.makedirs(os.path.dirname(path), exist_ok=True)
+                with open(path, 'w') as outputFile:
+                    outputFile.write(json.dumps(jsonData, indent=2))
+                        
+
+        path = os.path.join(os.path.join('template', 'template_pool.json'))
+        with open(path, 'r+') as file:
+            jsonData = json.load(file)
+            jsonData['name'] = f"repurposed_structures:villages/{variant}/houses"
+            for piece_name in piece_names:
+                jsonData['elements'].append({
+                    "weight": 6,
+                    "element": {
+                        "location": f"{mod_namespace}:villages/{variant}/{piece_name}",
+                        "processors": f"{structure_variants[variant]}",
+                        "projection": "rigid",
+                        "element_type": "minecraft:legacy_single_pool_element"
+                    }
+                })
+            path = os.path.join(datapack_location, os.path.join(data_location, 'repurposed_structures', 'pool_additions', 'villages', variant, 'houses.json'))
+            if not exists(path):
+                os.makedirs(os.path.dirname(path), exist_ok=True)
+                with open(path, 'w') as outputFile:
+                    outputFile.write(json.dumps(jsonData, indent=2))
+                        
+        os.makedirs(os.path.dirname(os.path.join(data_location, mod_namespace, "structures", "villages", variant, "")), exist_ok=True)
+
+
+    print('\n\nFINISHED!')
+    print('\nRESTARTING!\n\n')
